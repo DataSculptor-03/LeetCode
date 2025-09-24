074f991cdca2b4f8f5cf7e23adb9e2c1be3ce2cb
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-09-24 19:51:29 +0530

    Create 3512. Minimum Operations to Make Array Sum Divisible by K.py

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        t=sum(nums)
+        rem=t%k
+        return rem
