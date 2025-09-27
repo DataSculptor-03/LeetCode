7be7b611031bef0ffe0d56a8cb9c474d535ef92f
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-09-27 19:55:54 +0530

    Create 1662. Check If Two String Arrays are Equivalent.py

---
+++ 1662. Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        a=''
+        b=''
+        for i in word1:
+            a+=i
+        for i in word2:
+            b+=i
+        return a==b
