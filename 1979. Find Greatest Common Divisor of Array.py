ad70c2048757c3917bd4737cda39a7dd65084aed
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-10-09 22:24:44 +0530

    Create 1979. Find Greatest Common Divisor of Array.py

---
+++ 1979. Find Greatest Common Divisor of Array.py
@@ -0,0 +1,13 @@
+import math
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        return gcd(min(nums),max(nums))
