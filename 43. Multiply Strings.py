62f098a9c7d2cc1818a8ef3124b2227b0651bf55
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-11-17 21:55:25 +0530

    Create 43. Multiply Strings.py

---
+++ 43. Multiply Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        i1=int(num1)
+        i2=int(num2)
+        res=str(i1*i2)
+        return res
