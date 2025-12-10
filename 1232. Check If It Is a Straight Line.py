cd41c9ad03b1364ef9f41f87472fc5ad5ccdef15
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-12-10 19:06:36 +0530

    Create 1232. Check If It Is a Straight Line.py

---
+++ 1232. Check If It Is a Straight Line.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+        dx = x1 - x0
+        dy = y1 - y0
+        
+        for i in range(2, len(coordinates)):
+            x, y = coordinates[i]
+            if (y - y0) * dx != (x - x0) * dy:
+                return False
+        return True
