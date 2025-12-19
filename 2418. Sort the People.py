5da3ea95024201e5ad026aec873a946adf601f27
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2025-12-19 21:33:22 +0530

    Create 2418. Sort the People.py

---
+++ 2418. Sort the People.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [name for name, _ in sorted(zip(names, heights), key=lambda x: x[1], reverse=True)]
