1307eb074a620197b20c9ac57c9c59c58a8542bf
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-01-01 19:09:10 +0530

    Create 1619. Mean of Array After Removing Some Elements.py

---
+++ 1619. Mean of Array After Removing Some Elements.py
@@ -0,0 +1,9 @@
+from typing import List
+import statistics
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        arr.sort()
+        n = len(arr)
+        k = n // 20   
+        trimmed = arr[k : n-k]   
+        return statistics.mean(trimmed)
