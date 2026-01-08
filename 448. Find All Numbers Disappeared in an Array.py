36608d582e689e02c0813c1ad56e16cc20cbf91c
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-01-08 13:19:04 +0530

    Create 448. Find All Numbers Disappeared in an Array.py

---
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        n = len(nums)
+        return [i for i in range(1, n + 1) if i not in s]
