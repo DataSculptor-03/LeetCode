29b71cb334af305a44216953a2c909dd70b57a7d
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-02-05 19:59:13 +0530

    Create 961. N-Repeated Element in Size 2N Array.py

---
+++ 961. N-Repeated Element in Size 2N Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        l=len(nums)//2
+        s=set(nums)
+        for i in s:
+            if nums.count(i)==l:
+                return i
