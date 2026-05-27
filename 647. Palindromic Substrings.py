680877b94d28ebeedfd3735f7bc960d0fdc082f0
DataSculptor-03 <136855671+DataSculptor-03@users.noreply.github.com>
2026-05-28 01:30:54 +0530

    Create 647. Palindromic Substrings.py

---
+++ 647. Palindromic Substrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count=0
+        for i in range(0,len(s)):
+            for j in range(i+1,len(s)+1):
+                st=s[i:j]
+                if st==st[::-1]:
+                    count+=1
+        return count
